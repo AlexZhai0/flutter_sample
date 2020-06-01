f1a07a3ed71ec28c510ab235850629d6f9bebc58
Alex <253324826@qq.com>
2020-06-01 21:00:36 +0800

    add ignore rules and setting files

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/sno/Library/Flutter/sdk"
-export "FLUTTER_APPLICATION_PATH=/Users/sno/FlutterProjects/flutter_sample"
-export "FLUTTER_TARGET=/Users/sno/FlutterProjects/flutter_sample/lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/sno/projects/flutter/flutter_sample"
+export "FLUTTER_TARGET=/Users/sno/projects/flutter/flutter_sample/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_FRAMEWORK_DIR=/Users/sno/Library/Flutter/sdk/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
